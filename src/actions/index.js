eec5828a22765d35aacf058ca5ab562ef1a8d659
Kyle L Frisbie <kyle.l.frisbie@gmail.com>
2016-05-16 20:21:11 -0600

    lecture 57, redux promise

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = '64d56cc68ca24116df8ce48440785bc8';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?&appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+	const url = `${ROOT_URL}&q=${city},us`;
+	const request = axios.get(url);
+	
+	return {
+		type: FETCH_WEATHER,
+		payload: request
+	};
+}
